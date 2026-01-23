664a83bc81a47e1974d59f7ffc238376c011e391
wahb-amir <op422010@gmail.com>
2026-01-23 14:45:01 +0500

    Contact form

--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ import Feature from "./Components/Feature";
 import Workflow from "./Components/Workflow";
 import Pricing from "./Components/Pricing";
 import Testimonial from './Components/Testimonial'
+import Contact from "./Components/Contact";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -14,6 +16,7 @@ export default function Home() {
         <Feature/>
         <Pricing/>
         <Testimonial/>
+        <Contact/>
       </div>
     </div>
   );
